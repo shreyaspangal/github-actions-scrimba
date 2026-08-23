862bc00cf3e11426d44cd0b87ff76bd3010ae311
shreyaspangal <shreyaspangal03@gmail.com>
2026-08-23 18:05:28 +0530

    refactor app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    const msg = "Works on my machine.";
+    const item = "machine";
+    const msg = `Works on my ${item}.`;
     res.send(msg);
 });
 
