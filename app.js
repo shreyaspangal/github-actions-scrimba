f995854838ffe2cfd5c231a7ac3b5c97c239ae47
shreyaspangal <shreyaspangal03@gmail.com>
2026-08-23 14:28:16 +0530

    fix: failed lint pass

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    let msg = "Works on my machine.";
+    const msg = "Works on my machine.";
     res.send(msg);
 });
 
