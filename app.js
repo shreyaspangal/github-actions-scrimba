8f65c29620ca0b79bbc395294f20e332d2efce3d
shreyaspangal <shreyaspangal03@gmail.com>
2026-08-23 14:24:30 +0530

    add: eslint setup and config with failed test case

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Works on my machine.");
+    let msg = "Works on my machine.";
+    res.send(msg);
 });
 
 module.exports = app;
